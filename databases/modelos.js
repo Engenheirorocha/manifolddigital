732a2af447a9306281d3badd192defbdfcbbc93d
Engenheirorocha <thiagorinnai@gmail.com>
2026-05-22 23:01:37 -0300

    Update modelos.js

--- databases/modelos.js
+++ databases/modelos.js
@@ -1,213 +1,39 @@
-/* HVAC PRO - databases/modelos.js */
+/* HVAC PRO - databases/modelos.js
+   Extraído do HTML principal original. */
 
-window.HVAC_MODELOS = {
-  errorCategories: [
-    {
-      name: "Split",
-      search: ["split", "hi wall", "evaporadora", "parede"],
-      icon: "split"
-    },
-    {
-      name: "Cassete",
-      search: ["cassete", "casete", "teto", "4 vias", "quatro vias"],
-      icon: "cassete"
-    },
-    {
-      name: "Janela",
-      search: ["janela", "ar janela", "ar de janela"],
-      icon: "janela"
-    },
-    {
-      name: "Piso Teto",
-      search: ["piso teto", "piso-teto", "piso", "teto"],
-      icon: "pisoTeto"
-    }
-  ],
+window.errorCategories = [
+  {name:"Split",search:["split","evaporadora"],icon:svgSplit()},
+  {name:"Cassete",search:["cassete","casete","teto"],icon:svgCassete()},
+  {name:"Janela",search:["janela","ar janela","ar de janela"],icon:svgJanela()},
+  {name:"Piso Teto",search:["piso teto","piso-teto","piso","teto"],icon:svgPisoTeto()}
+];
 
-  brandsByCategory: {
-    "Split": [
-      "Midea",
-      "Springer",
-      "Carrier",
-      "Samsung",
-      "LG",
-      "Gree",
-      "Elgin",
-      "Daikin",
-      "Fujitsu",
-      "Panasonic",
-      "Philco",
-      "Electrolux",
-      "Consul",
-      "TCL",
-      "Agratto",
-      "Komeco",
-      "Hisense"
-    ],
+window.brandsByCategory = {
+  "Split":["Midea","Springer","Carrier","Samsung","LG","Gree","Elgin","Daikin","Fujitsu","Panasonic","Philco","Electrolux","Consul","TCL","Agratto","Komeco","Hisense"],
+  "Cassete":["Midea","Carrier","Samsung","LG","Gree","Daikin","Fujitsu","Electrolux","Hitachi","Trane","York"],
+  "Janela":["Consul","Springer","Midea","Electrolux","Philco","Gree","Elgin"],
+  "Piso Teto":["Midea","Carrier","LG","Gree","Elgin","Daikin","Fujitsu","TCL","Komeco","Hitachi","Trane","York"]
+};
 
-    "Cassete": [
-      "Midea",
-      "Carrier",
-      "Samsung",
-      "LG",
-      "Gree",
-      "Daikin",
-      "Fujitsu",
-      "Electrolux",
-      "Hitachi",
-      "Trane",
-      "York"
-    ],
-
-    "Janela": [
-      "Consul",
-      "Springer",
-      "Midea",
-      "Electrolux",
-      "Philco",
-      "Gree",
-      "Elgin"
-    ],
-
-    "Piso Teto": [
-      "Midea",
-      "Carrier",
-      "LG",
-      "Gree",
-      "Elgin",
-      "Daikin",
-      "Fujitsu",
-      "TCL",
-      "Komeco",
-      "Hitachi",
-      "Trane",
-      "York"
-    ]
-  },
-
-  modelsByBrand: {
-    "Midea": [
-      "Xtreme Save",
-      "AirVolution",
-      "Springer Midea",
-      "Liva",
-      "Inverter",
-      "Cassete Inverter",
-      "Piso Teto Inverter"
-    ],
-
-    "Springer": [
-      "AirVolution",
-      "Midea Springer",
-      "Janela Eletrônico",
-      "Janela Mecânico"
-    ],
-
-    "Carrier": [
-      "XPower",
-      "Inverter Carrier",
-      "Piso Teto Space",
-      "Cassete Carrier"
-    ],
-
-    "Samsung": [
-      "WindFree",
-      "Digital Inverter",
-      "Max Plus",
-      "Cassete 4 Vias"
-    ],
-
-    "LG": [
-      "Dual Inverter",
-      "ArtCool",
-      "Voice",
-      "UV Nano",
-      "Cassete Inverter",
-      "Piso Teto Inverter"
-    ],
-
-    "Gree": [
-      "Eco Garden",
-      "G-Top",
-      "G-Diamond",
-      "Cassete Gree"
-    ],
-
-    "Elgin": [
-      "Eco Inverter",
-      "Hi Wall Eco",
-      "Piso Teto Elgin"
-    ],
-
-    "Daikin": [
-      "Advance",
-      "EcoSwing",
-      "Cassete SkyAir",
-      "Piso Teto Daikin"
-    ],
-
-    "Fujitsu": [
-      "Hi Wall Inverter",
-      "Cassete Fujitsu",
-      "Piso Teto Fujitsu"
-    ],
-
-    "Panasonic": [
-      "Econavi",
-      "Inverter Panasonic"
-    ],
-
-    "Philco": [
-      "PAC Inverter",
-      "Janela Philco"
-    ],
-
-    "Electrolux": [
-      "Eco Turbo",
-      "Janela Eletrônico",
-      "Cassete Electrolux"
-    ],
-
-    "Consul": [
-      "Facilite",
-      "Bem Estar",
-      "Janela Consul"
-    ],
-
-    "TCL": [
-      "Elite Series",
-      "TAC Inverter"
-    ],
-
-    "Agratto": [
-      "Neo Top",
-      "One Top"
-    ],
-
-    "Komeco": [
-      "Komeco Inverter",
-      "Piso Teto Komeco"
-    ],
-
-    "Hisense": [
-      "Energy Pro",
-      "Hi Wall Inverter"
-    ],
-
-    "Hitachi": [
-      "Set Free",
-      "Cassete Hitachi",
-      "Piso Teto Hitachi"
-    ],
-
-    "Trane": [
-      "Cassete Trane",
-      "Piso Teto Trane"
-    ],
-
-    "York": [
-      "Cassete York",
-      "Piso Teto York"
-    ]
-  }
+window.modelsByBrand = {
+  "Midea":["Xtreme Save","AirVolution","Springer Midea","Liva","Inverter","Cassete Inverter","Piso Teto Inverter"],
+  "Springer":["AirVolution","Midea Springer","Janela Eletrônico","Janela Mecânico"],
+  "Carrier":["XPower","Inverter Carrier","Piso Teto Space","Cassete Carrier"],
+  "Samsung":["WindFree","Digital Inverter","Max Plus","Cassete 4 Vias"],
+  "LG":["Dual Inverter","ArtCool","Voice","UV Nano","Cassete Inverter","Piso Teto Inverter"],
+  "Gree":["Eco Garden","G-Top","G-Diamond","Cassete Gree"],
+  "Elgin":["Eco Inverter","Hi Wall Eco","Piso Teto Elgin"],
+  "Daikin":["Advance","EcoSwing","Cassete SkyAir","Piso Teto Daikin"],
+  "Fujitsu":["Hi Wall Inverter","Cassete Fujitsu","Piso Teto Fujitsu"],
+  "Panasonic":["Econavi","Inverter Panasonic"],
+  "Philco":["PAC Inverter","Janela Philco"],
+  "Electrolux":["Eco Turbo","Janela Eletrônico","Cassete Electrolux"],
+  "Consul":["Facilite","Bem Estar","Janela Consul"],
+  "TCL":["Elite Series","TAC Inverter"],
+  "Agratto":["Neo Top","One Top"],
+  "Komeco":["Komeco Inverter","Piso Teto Komeco"],
+  "Hisense":["Energy Pro","Hi Wall Inverter"],
+  "Hitachi":["Set Free","Cassete Hitachi","Piso Teto Hitachi"],
+  "Trane":["Cassete Trane","Piso Teto Trane"],
+  "York":["Cassete York","Piso Teto York"]
 };
